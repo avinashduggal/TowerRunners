ed4f1307ca63bc1513ac18d4c2d39c8eef209140
Avinash Duggal <avinashduggal4@gmail.com>
2025-02-21 21:45:44 -0800

    Update train_rl.py

--- Project/train_rl.py
+++ Project/train_rl.py
@@ -11,13 +11,9 @@ policy_name = "CnnPolicy"
 experiment_logdir = f"OT_logs/{policy_name}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 env = ObstacleTowerEnv('./ObstacleTower/obstacletower', retro=True, realtime_mode=False)
 env = Monitor(env, filename=experiment_logdir)
-
-# Wrap the environment in a DummyVecEnv for stable-baselines
 env = DummyVecEnv([lambda: env])
 
-# Now, the PPO model should work with the wrapped environment
 model = PPO(policy_name, env, verbose=1)
 model.learn(total_timesteps=100000)
 
-# Save the model
 model.save(f"models/{policy_name}_obstacle_tower_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
