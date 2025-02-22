65b909f52669ae2517fb027886c8bc730624c3f3
Avinash Duggal <avinashduggal4@gmail.com>
2025-02-21 21:42:30 -0800

    Update train_rl.py

--- Project/train_rl.py
+++ Project/train_rl.py
@@ -1,37 +1,23 @@
+from datetime import datetime
 import gym
+import numpy as np
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv  # Correct import
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
+from gym.spaces import Box
 from obstacle_tower_env import ObstacleTowerEnv
 
-class ObstacleTowerEnvWrapper(gym.Env):
-    def __init__(self, env_path):
-        self.env = ObstacleTowerEnv(env_path, retro=False)
-        self.observation_space = self.env.observation_space[0]  # Assuming it's an image
-        self.action_space = self.env.action_space
-
-    def reset(self):
-        obs = self.env.reset()
-        return obs[0]  # Return only the visual observation
-
-    def step(self, action):
-        obs, reward, done, info = self.env.step(action)
-        return obs[0], reward, done, info  # Return only the visual observation
-
-    def render(self, mode='human'):
-        return self.env.render(mode)
-
-    def close(self):
-        self.env.close()
-
-# Initialize the environment with the custom wrapper
-env = ObstacleTowerEnvWrapper('./ObstacleTower/obstacletower.x86_64')
+policy_name = "CnnPolicy"
+experiment_logdir = f"OT_logs/{policy_name}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+env = ObstacleTowerEnv('./ObstacleTower/obstacletower', retro=True, realtime_mode=False)
+env = Monitor(env, filename=experiment_logdir)
 
 # Wrap the environment in a DummyVecEnv for stable-baselines
 env = DummyVecEnv([lambda: env])
 
 # Now, the PPO model should work with the wrapped environment
-model = PPO("MlpPolicy", env, verbose=1)  # Use CnnPolicy
+model = PPO(policy_name, env, verbose=1)
 model.learn(total_timesteps=100000)
 
 # Save the model
-model.save("ppo_obstacle_tower")
+model.save(f"models/{policy_name}_obstacle_tower_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
