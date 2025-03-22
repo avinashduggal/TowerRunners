07ba925df85fe9044ba35ca2aff853506521c0e3
patrickwong28 <143467643+patrickwong28@users.noreply.github.com>
2025-03-21 21:02:53 -0700

    Update submit_ppo_obstacle_tower.sh

--- Project/submit_ppo_obstacle_tower.sh
+++ Project/submit_ppo_obstacle_tower.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A cs175_class_gpu    ## Account to charge
-#SBATCH --time=04:00:00       ## Maximum running time of program
+#SBATCH --time=12:00:00       ## Maximum running time of program
 #SBATCH --nodes=1             ## Number of nodes.
                               ## Set to 1 if you are using GPU.
 #SBATCH --partition=gpu       ## Partition name
@@ -8,4 +8,7 @@
 #SBATCH --cpus-per-task 8    ## Number of CPU cores
 #SBATCH --gres=gpu:V100:1     ## Type and the number of GPUs
 
-xvfb-run python train_rl.py
+Xvfb :3 -screen 0 1024x768x24 & # Create virtual display so the remote machine can run the python script
+export DISPLAY=:3               # Redirect Unity to the virtual screen created
+
+python train_rl.py
